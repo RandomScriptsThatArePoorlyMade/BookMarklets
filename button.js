a8fd5e68294f545a68415021816ad6378d4011ae
PoorlyMadeScripts <107751290+RandomScriptsThatArePoorlyMade@users.noreply.github.com>
2023-09-04 16:54:43 -0700

    button.js
    Makes a clickable blank canvas that does nothing

---
+++ button.js
@@ -0,0 +1,15 @@
+javascript: (function() {
+    var button = document.createElement('div');
+    button.style.width = '10000px';
+    button.style.height = '10000px';
+    button.style.backgroundColor = 'white';
+    button.style.color = 'black';
+    button.style.lineHeight = '50px';
+    button.style.position = 'fixed';
+    button.style.top = '50%';
+    button.style.left = '50%';
+    button.style.transform = 'translate(-50%, -50%)';
+    button.style.zIndex = '9999';
+    button.style.cursor = 'pointer';
+    document.body.appendChild(button);
+})();
