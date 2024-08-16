b2228fa46ccfe982fd59053312d9ec621f795207
PoorlyMadeScripts <107751290+RandomScriptsThatArePoorlyMade@users.noreply.github.com>
2024-08-16 01:06:31 -0700

    Update and rename PaintR.js to PaintRR.js
    Reworked the "infobar," its now a much larger square that is closable. Added a couple things like the ability to set any line size and to set any line size increment. Good for now :). You can run this on most webpages with a bookmark, create a book mark and set the url to this, "javascript: (function(){var script = document.createElement("script");  script.src = "//cdn.jsdelivr.net/gh/RandomScriptsThatArePoorlyMade/BookMarklets@Scripts/PaintRR.js"; script.crossOrigin = "anonymous";  document.body.appendChild(script);})();"

--- PaintRR.js
+++ PaintRR.js
@@ -2,40 +2,47 @@
     var r = 50;
     var g = 50;
     var b = 50;
+    var lineSizeIncrement = 1;
     var lifted = false;
     var pressnumber = 0;
+    var pressnumber2 = 0;
     var lineSize = 50;
     var infobar = document.createElement("div");
     infobar.style.position = "fixed";
     infobar.style.background = "black";
     infobar.style.opacity = '0.8';
-    infobar.style.height = "25px";
-    infobar.style.width = window.innerWidth + 'px';
-    infobar.style.right = "0%";
-    infobar.style.bottom = "0%";
-    infobar.style.zIndex = '9999999999';
+    infobar.style.height = "375px";
+    infobar.style.width = "775px";
+    infobar.style.left = "0%";
+    infobar.style.top = "0%";
+    infobar.style.border = "5px solid black";
+    infobar.style.borderBottomRightRadius = "75px";
+    infobar.style.zIndex = '9999999999999';
     document.body.appendChild(infobar);
     var info = document.createElement("div");
     info.style.position = "fixed";
+    info.style.right = "51%";
     info.style.left = "1%";
-    info.style.bottom = "1%";
-    info.style.fontSize = "10px";
+    info.style.top = "2%";
+    info.style.fontSize = "18px";
     info.style.color = "white";
     info.style.textShadow = "1px 1px black";
-    info.style.zIndex = '9999999999';
+    info.style.zIndex = '9999999999999';
 
     function updateInfo() {
-        info.innerHTML = "Press the 'P' key to start painting, if you want to clear use 'C', to 'lift' or break a line use 'L', to modify RGB values use 'Z', to decrease line size use '[', to increase line size use ']', current RBG values are R: " + r + ", G: " + g + ", B: " + b + ", current line size is: " + lineSize + "px";
+        info.innerHTML = "Press the 'P' key to start painting.<br><br> If you want to clear the canvas use 'C'<br> To 'lift' or break a line use 'L' <br>To modify RGB values use 'Z'<br>To decrease line size use '['  To increase line size use ']'  To modify line size increment use '/'<br> To enter a specific size use 'S'<br><br><br> Current RBG values are R: " + r + "  G: " + g + "  B: " + b + "<br> Current line size is: " + lineSize + "px<br> Current line size increment is: " + lineSizeIncrement + "px <br><br><br><br>To hide this information press 'i'";
     }
     updateInfo();
     document.body.appendChild(info);
     var dot = document.createElement("div");
-    function updateLineSize () {
-                    dot.style.width = lineSize + 'px';
-                    dot.style.height = lineSize + 'px';
-                    }
+
+    function updateLineSize() {
+        dot.style.width = lineSize + 'px';
+        dot.style.height = lineSize + 'px';
+    }
+
     function updateColor() {
-      dot.style.backgroundColor = `rgb(${r},${g},${b})`;
+        dot.style.backgroundColor = `rgb(${r},${g},${b})`;
     }
     document.addEventListener("keydown", function(event) {
         switch (event.keyCode) {
@@ -46,12 +53,10 @@
                     dot.style.position = "absolute";
                     dot.style.zIndex = '9999';
                     document.addEventListener("mousemove", function(e) {
-
                         var mouseX = e.clientX;
                         var mouseY = e.clientY;
                         dot.style.left = mouseX + "px";
                         dot.style.top = mouseY + "px";
-
                     });
                     dot.style.backgroundColor = `rgb(${r},${g},${b})`;
                     document.body.appendChild(dot);
@@ -82,7 +87,6 @@
                 break;
         }
     });
-    
 
     function clearAllDots() {
         var dots = document.querySelectorAll('.dot');
@@ -94,8 +98,10 @@
                 clearAllDots();
                 break;
             case 76:
-                lifted = true;
                 pressnumber++;
+                if (pressnumber == 1) {
+                    lifted = true;
+                }
                 if (pressnumber == 2) {
                     lifted = false;
                     pressnumber = 0;
@@ -109,12 +115,34 @@
                 updateInfo();
                 break;
             case 219:
-                lineSize -= 0.5;
+                lineSize -= lineSizeIncrement;
                 updateLineSize();
                 updateInfo();
                 break;
             case 221:
-                lineSize += 0.5;
+                lineSize = +lineSize + +lineSizeIncrement;
+                updateLineSize();
+                updateInfo();
+                break;
+            case 73:
+                pressnumber2++;
+                if (pressnumber2 == 1) {
+                    document.body.removeChild(info);
+                    document.body.removeChild(infobar);
+                }
+                if (pressnumber2 == 2) {
+                    document.body.appendChild(infobar);
+                    document.body.appendChild(info);
+                    pressnumber2 = 0;
+                }
+                break;
+            case 191:
+                lineSizeIncrement = prompt("Enter desired line size increment");
+                updateLineSize();
+                updateInfo();
+                break;
+            case 83:
+                lineSize = prompt("Enter desired line size");
                 updateLineSize();
                 updateInfo();
                 break;
